ecaa121e71472e2dcbe38981b12aa74af210baa5
AndyNoob <comfortableandy9082@gmail.com>
2024-08-02 18:51:54 -0700

    Add more requires to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,6 @@ module brew.main {
     requires lombok;
     requires org.jetbrains.annotations;
     requires org.joml;
+    requires net.kyori.adventure.text.minimessage;
+    requires net.kyori.adventure;
 }
