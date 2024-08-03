6b1cb8d56220a5fa9f9c84371901f9ba94a6cfc7
AndyNoob <comfortableandy9082@gmail.com>
2024-08-02 22:51:16 -0700

    Make MenuAction#tryRun ask for relative clicked position and add builder to ActionCriteria

--- src/main/java/comfortable_andy/brew/menu/actions/MenuAction.java
+++ src/main/java/comfortable_andy/brew/menu/actions/MenuAction.java
@@ -1,5 +1,6 @@
 package comfortable_andy.brew.menu.actions;
 
+import lombok.Builder;
 import org.bukkit.entity.HumanEntity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -7,8 +8,9 @@ import org.joml.Vector2i;
 
 public interface MenuAction {
 
-    boolean tryRun(HumanEntity entity, Vector2i screenPosition);
+    boolean tryRun(HumanEntity entity, Vector2i relativePosition);
 
+    @Builder
     public record ActionCriteria(@NotNull ActionType type, @Nullable ActionModifier modifier) {
     }
 
